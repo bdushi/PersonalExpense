4ff324e3782f403c6be34baea29d4793e532ab62
brddev <brddev@gmail.com>
2018-11-05 21:52:49 +0100

    Querry error

--- app/src/main/java/al/bruno/financaime/data/source/local/dao/BudgetDao.kt
+++ app/src/main/java/al/bruno/financaime/data/source/local/dao/BudgetDao.kt
@@ -20,9 +20,6 @@ interface BudgetDao {
     @Query("UPDATE budget SET _incomes = :incomes WHERE _id = :id")
     fun updateIncomes(incomes: Double, id:Long) //: Completable //Single<Int>
 
-    /*@Query("SELECT b._id AS _id, b._budget AS _budget, b._incomes AS _incomes, b._date AS _date , SUM(e._expense) AS _expense " +
-            "FROM budget AS b LEFT JOIN expense AS e ON b._id = e._id_budget " +
-            "WHERE strftime('%m', datetime(b._date/1000, 'unixepoch')) = :month GROUP BY e._id_budget")*/
-    @Query("SELECT * FROM budget AS b WHERE strftime('%m', datetime(b._date/1000, 'unixepoch')) = :month")
+    @Query("SELECT b._id AS _id, (b._budget - (SELECT TOTAL(e._value) FROM budget AS b JOIN expense AS e WHERE e._id_budget = b._id GROUP BY e._id_budget)) AS _budget, b._incomes AS _incomes, b._date AS _date FROM budget AS b WHERE strftime('%m', datetime(b._date/1000, 'unixepoch')) = :month")
     fun budget(month: String) : LiveData<Budget>
 }
