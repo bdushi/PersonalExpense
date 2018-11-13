dd9b3cf00cbfc3eb795077033aa67d102f7957f8
bdushi <brunodushi@gmail.com>
2018-11-13 17:27:42 +0100

    Workgin with BudgetMaster object

--- app/src/main/java/al/bruno/financaime/data/source/local/BudgetMasterLocalDataSource.kt
+++ app/src/main/java/al/bruno/financaime/data/source/local/BudgetMasterLocalDataSource.kt
@@ -2,10 +2,26 @@ package al.bruno.financaime.data.source.local
 
 import al.bruno.financaime.data.source.BudgetMasterDataSource
 import al.bruno.financaime.model.BudgetMaster
+import android.content.Context
 import io.reactivex.Single
 
-class BudgetMasterLocalDataSource : BudgetMasterDataSource {
+class BudgetMasterLocalDataSource(context: Context) : BudgetMasterDataSource {
+    private val DATABASE_INSTANCE: AppDatabase
+
+    init {
+        DATABASE_INSTANCE = AppDatabase.getInstance(context)
+    }
+
+    companion object {
+        var INSTANCE: BudgetMasterLocalDataSource? = null
+        fun INSTANCE(context: Context): BudgetMasterLocalDataSource {
+            if (INSTANCE == null)
+                INSTANCE = BudgetMasterLocalDataSource(context)
+            return INSTANCE as BudgetMasterLocalDataSource
+        }
+    }
+
     override fun budget(month: String): Single<List<BudgetMaster>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return INSTANCE.budgetMasterDao().budget(month)
     }
 }
