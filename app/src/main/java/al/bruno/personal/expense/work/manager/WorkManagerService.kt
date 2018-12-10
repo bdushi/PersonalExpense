faf9b47cdf09ca84d05cc6136c0682e6f483cc67
bdushi <brunodushi@gmail.com>
2018-12-10 15:13:19 +0100

    Updated WorkManager

--- app/src/main/java/al/bruno/personal/expense/work/manager/WorkManagerService.kt
+++ app/src/main/java/al/bruno/personal/expense/work/manager/WorkManagerService.kt
@@ -4,6 +4,7 @@ import al.bruno.personal.expense.dependency.injection.InjectionProvider.provideB
 import al.bruno.personal.expense.dependency.injection.InjectionProvider.providerSettingsInjection
 import al.bruno.personal.expense.model.Budget
 import android.content.Context
+import androidx.work.Result
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import io.reactivex.disposables.CompositeDisposable
@@ -11,22 +12,20 @@ import io.reactivex.schedulers.Schedulers
 import java.util.*
 
 class WorkManagerService(val context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
-    private val calendar = Calendar.getInstance()
-    private val disposable = CompositeDisposable()
     override fun doWork(): Result {
         if((
                         calendar[Calendar.MONTH] == 1 ||
-                        calendar[Calendar.MONTH] == 2 ||
-                        calendar[Calendar.MONTH] == 3 ||
-                        calendar[Calendar.MONTH] == 4 ||
-                        calendar[Calendar.MONTH] == 5 ||
-                        calendar[Calendar.MONTH] == 6 ||
-                        calendar[Calendar.MONTH] == 7 ||
-                        calendar[Calendar.MONTH] == 8 ||
-                        calendar[Calendar.MONTH] == 9 ||
-                        calendar[Calendar.MONTH] == 10 ||
-                        calendar[Calendar.MONTH] == 11 ||
-                        calendar[Calendar.MONTH] == 12) && calendar[Calendar.DAY_OF_MONTH] == 1) {
+                                calendar[Calendar.MONTH] == 2 ||
+                                calendar[Calendar.MONTH] == 3 ||
+                                calendar[Calendar.MONTH] == 4 ||
+                                calendar[Calendar.MONTH] == 5 ||
+                                calendar[Calendar.MONTH] == 6 ||
+                                calendar[Calendar.MONTH] == 7 ||
+                                calendar[Calendar.MONTH] == 8 ||
+                                calendar[Calendar.MONTH] == 9 ||
+                                calendar[Calendar.MONTH] == 10 ||
+                                calendar[Calendar.MONTH] == 11 ||
+                                calendar[Calendar.MONTH] == 12) && calendar[Calendar.DAY_OF_MONTH] == 1) {
             disposable.addAll(providerSettingsInjection(context)!!.settings(1).subscribeOn(Schedulers.io()).subscribe({ b ->
                 val budget = Budget()
                 budget.budget = b.budget
@@ -39,11 +38,14 @@ class WorkManagerService(val context: Context, workerParams: WorkerParameters) :
             }, {
 
             }))
-            return Result.SUCCESS
+            return Result.success()
         } else {
-            return Result.FAILURE
+            return Result.failure()
         }
     }
+
+    private val calendar = Calendar.getInstance()
+    private val disposable = CompositeDisposable()
 
     override fun onStopped() {
         disposable.clear()
