6c3ddc3f834ceb5d422a6024dfc6b6e0e7ce2681
bdushi <brunodushi@gmail.com>
2018-12-10 15:09:12 +0100

    Removed MaterialCalendarView

--- app/src/main/java/al/bruno/personal/expense/DetailsFragment.kt
+++ app/src/main/java/al/bruno/personal/expense/DetailsFragment.kt
@@ -6,7 +6,6 @@ import al.bruno.personal.expense.databinding.ExpenseSingleItemBinding
 import al.bruno.personal.expense.databinding.FragmentDetailsBinding
 import al.bruno.personal.expense.model.Expense
 import al.bruno.personal.expense.util.Utilities
-import al.bruno.personal.expense.view.model.ExpenseDetailsViewModel
 import al.bruno.personal.expense.view.model.ExpenseViewModel
 import android.os.Bundle
 import android.util.Log
@@ -19,8 +18,6 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import al.bruno.personal.expense.util.EventDecorator
-import org.threeten.bp.DateTimeUtils
 
 class DetailsFragment : Fragment() {
     private val disposable : CompositeDisposable = CompositeDisposable()
@@ -51,7 +48,7 @@ class DetailsFragment : Fragment() {
                         .date()
                         .subscribeOn(Schedulers.io())
                         .subscribe({
-                            fragmentDetailsBinding.expenseLogCalendarView.addDecorator(EventDecorator(R.color.red_a700, it))
+                            //fragmentDetailsBinding.expenseLogCalendarView.addDecorator(EventDecorator(R.color.red_a700, it))
                             Log.i(DetailsFragment::class.java.name, it.toString())
                         }, {
                             Log.i(DetailsFragment::class.java.name, it.message)
@@ -66,31 +63,27 @@ class DetailsFragment : Fragment() {
                 },{
                     Log.i(DetailsFragment::class.java.name, it.message)
                 }))*/
-        fragmentDetailsBinding.expenseLogCalendarView.setOnDateChangedListener { widget, date, selected ->
-            //DateTimeUtils.toDate(Instant.from(date.date).atZone(ZoneId.systemDefault()).toInstant())
-            //DateTimeUtils.toDate(Instant.from(LocalDate.of(date.year, date.month, date.day)).atZone(ZoneId.systemDefault()).toInstant())
-            disposable.addAll(ViewModelProviders.of(this)[ExpenseViewModel::class.java]
-                    .expenses(DateTimeUtils.toSqlDate(date.date))
-                    .subscribeOn(Schedulers.io())
-                    .subscribe({
-                        fragmentDetailsBinding.adapter = CustomAdapter(it, R.layout.expense_single_item, object : BindingData<Expense, ExpenseSingleItemBinding> {
-                            override fun bindData(t: Expense, vm: ExpenseSingleItemBinding) {
-                                vm.expense = t
-                            }
-                        })
-                    },{
-                        Log.i(DetailsFragment::class.java.name, it.message)
-                    }),
-                    ViewModelProviders.of(this)[ExpenseViewModel::class.java]
-                    .total(DateTimeUtils.toSqlDate(date.date))
-                    .subscribeOn(Schedulers.io())
-                    .subscribe({
-                        fragmentDetailsBinding.total = it
-                    },{
-                        fragmentDetailsBinding.total = null
-                        Log.i(DetailsFragment::class.java.name, it.message)
-                    }))
-        }
+        /*disposable.addAll(ViewModelProviders.of(this)[ExpenseViewModel::class.java]
+                .expenses(DateTimeUtils.toSqlDate(date.date))
+                .subscribeOn(Schedulers.io())
+                .subscribe({
+                    fragmentDetailsBinding.adapter = CustomAdapter(it, R.layout.expense_single_item, object : BindingData<Expense, ExpenseSingleItemBinding> {
+                        override fun bindData(t: Expense, vm: ExpenseSingleItemBinding) {
+                            vm.expense = t
+                        }
+                    })
+                },{
+                    Log.i(DetailsFragment::class.java.name, it.message)
+                }),
+                ViewModelProviders.of(this)[ExpenseViewModel::class.java]
+                        .total(DateTimeUtils.toSqlDate(date.date))
+                        .subscribeOn(Schedulers.io())
+                        .subscribe({
+                            fragmentDetailsBinding.total = it
+                        },{
+                            fragmentDetailsBinding.total = null
+                            Log.i(DetailsFragment::class.java.name, it.message)
+                        }))*/
         return fragmentDetailsBinding.root;
     }
 
--- app/src/main/java/al/bruno/personal/expense/util/Constants.kt
+++ app/src/main/java/al/bruno/personal/expense/util/Constants.kt
@@ -1,4 +1,3 @@
 package al.bruno.personal.expense.util
 
-
 val ACTION_PROCESS_UPDATES = " al.bruno.financaime.PROCESS_UPDATES"
