7f5479dc5819894be9ad267730ffcd0fe7fc10a4
bdushi <brunodushi@gmail.com>
2019-01-11 10:26:24 +0100

    working with Home ui

--- app/src/main/java/al/bruno/personal/expense/util/FabScrollBehavior.kt
+++ app/src/main/java/al/bruno/personal/expense/util/FabScrollBehavior.kt
@@ -9,7 +9,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class FabScrollBehavior (context: Context, attrs: AttributeSet): FloatingActionButton.Behavior(context, attrs) {
     override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
-        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
+        //return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
     }
     override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type)
@@ -20,8 +21,18 @@ class FabScrollBehavior (context: Context, attrs: AttributeSet): FloatingActionB
                     fab.visibility = View.INVISIBLE
                 }
             })
+            //child.animate().translationY((child.height + (child.layoutParams as CoordinatorLayout.LayoutParams).bottomMargin).toFloat()).setInterpolator(LinearInterpolator()).start()
         } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
             child.show()
+            //child.animate().translationY(0.toFloat()).setInterpolator(LinearInterpolator()).start();
         }
     }
+
+    /*override fun onStopNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, type: Int) {
+        super.onStopNestedScroll(coordinatorLayout, child, target, type)
+        Handler().postDelayed({
+            child.show()
+            //child.animate().translationY(0.toFloat()).setInterpolator(LinearInterpolator()).start()
+        }, 1000)
+    }*/
 }
