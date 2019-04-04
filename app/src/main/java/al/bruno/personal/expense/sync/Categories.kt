b82d1bc75e74839cb5c6aad1d74edbad98704e0c
bdushi <brunodushi@gmail.com>
2019-04-04 17:53:36 +0200

    Added default constructor

--- app/src/main/java/al/bruno/personal/expense/sync/Categories.kt
+++ app/src/main/java/al/bruno/personal/expense/sync/Categories.kt
@@ -2,4 +2,8 @@ package al.bruno.personal.expense.sync
 
 import androidx.room.ColumnInfo
 
-data class Categories constructor(@ColumnInfo(name = "_category") val category: String, @ColumnInfo(name = "_type") val type: String)
+data class Categories constructor(
+        @ColumnInfo(name = "_category") val category: String,
+        @ColumnInfo(name = "_type") val type: String) {
+    constructor() : this(category = "", type = "")
+}
--- app/src/main/java/al/bruno/personal/expense/sync/Expense.kt
+++ app/src/main/java/al/bruno/personal/expense/sync/Expense.kt
@@ -2,4 +2,11 @@ package al.bruno.personal.expense.sync
 
 import androidx.room.ColumnInfo
 
-data class Expense constructor(@ColumnInfo(name = "_type") var type: String, @ColumnInfo(name = "_category") var category: String, @ColumnInfo(name = "_memo") var memo: String, @ColumnInfo(name = "_amount") var amount: Double, @ColumnInfo(name = "_date") var date: Long)
+data class Expense constructor(
+        @ColumnInfo(name = "_type") var type: String,
+        @ColumnInfo(name = "_category") var category: String,
+        @ColumnInfo(name = "_memo") var memo: String,
+        @ColumnInfo(name = "_amount") var amount: Double,
+        @ColumnInfo(name = "_date") var date: Long) {
+    constructor() : this(type = "", category = "", memo = "", amount = 0.0, date = 0)
+}
